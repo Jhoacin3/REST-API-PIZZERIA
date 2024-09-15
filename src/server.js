a8abde7b6691608ba799754c2f8447c8cd527de8
jhoacin21project <jhoacin21project@gmail.com>
2024-09-15 02:27:34 -0600

    adjust server

--- src/server.js
+++ src/server.js
@@ -1,15 +1,13 @@
-const connection = require('./db.js');
 const express = require('express');
 require('dotenv').config();
 const app = express();
+const rolRouter = require("./routes/rol.router.js")
+app.use(express.json());
 
 const port = process.env.PORT || 3000;
 
-//Prueba de conexión a la base de datos.
-app.get('/ping', async (req, res) =>{
-   const [result] = await connection.query('SELECT 1 + 1 AS result')
-   res.json(result);
-});
+//registro de rutas
+app.use('/apiPizza', rolRouter);
 
 //Servidor escuchando...
 app.listen(port, () =>{
