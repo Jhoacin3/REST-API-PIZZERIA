a8730649cf50acedb827caf4f384063cf5eba892
jhoacin21project <jhoacin21project@gmail.com>
2025-01-25 20:49:45 -0600

    add structure for menu

---
+++ pizzeria_frontend/src/app/models/menu-item.model.ts.ts
@@ -0,0 +1,9 @@
+export interface MenuItemModelTs {
+    //Interfaz que define los atributos de un item del menús
+    id: number;
+    name: string;
+    description: string;
+    price: number;
+    id_category: number;
+
+}
