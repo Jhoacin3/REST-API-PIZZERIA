d7f29b771c7c6b40d743f4b05e03ef7510dac00e
jhoacin21project <jhoacin21project@gmail.com>
2025-01-29 01:12:37 -0600

    fix style home

--- pizzeria_frontend/src/app/features/dashboard/pages/home/home.component.ts
+++ pizzeria_frontend/src/app/features/dashboard/pages/home/home.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import {ChangeDetectionStrategy} from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [RouterOutlet, MatButtonModule, MatCardModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
