f98408f3cfc233c4b42caa8a2438dd77515535c3
jhoacin21project <jhoacin21project@gmail.com>
2025-01-29 01:14:22 -0600

    fix menu, add routerLink

--- pizzeria_frontend/src/app/features/dashboard/pages/menu-list/menu-list.component.ts
+++ pizzeria_frontend/src/app/features/dashboard/pages/menu-list/menu-list.component.ts
@@ -3,25 +3,44 @@ import { MenuItemModel } from '../../../../core/models/menu-item.model';
 import { MenuService } from '../../../../core/services/menu.service';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {MaterialModule} from '../../../../material-module';
 
 @Component({
   selector: 'app-menu-list',
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,MaterialModule],
   templateUrl: './menu-list.component.html',
   styleUrl: './menu-list.component.css'
 })
-export class MenuListComponent {
+export class MenuListComponent implements OnInit {
+  title = 'pizzeria_frontend';
+  errorMessage = '';
+  successMessage = '';
+
   menuItems: MenuItemModel[] = [];
   //Constructor de la clase
-  constructor(private menuService: MenuService) { }
+  constructor(private menuService: MenuService) { 
 
-  //Método que se ejecuta al iniciar el componente
+  }
+
   ngOnInit(): void {
-    this.menuService.getMenuItems().subscribe(data => {
-      console.log('Respuesta de la API:', data);
-
-      this.menuItems = data;
-    });
+    this.menuService.getMenuItems().subscribe({
+      next: (response: any) => {
+        if (response.success) {
+          this.menuItems = response.data;
+          this.successMessage = response.message;
+          this.errorMessage = ''; // Limpiamos posibles errores previos
+        }else{
+          this.errorMessage = response.message;
+          this.successMessage = ''; // Limpiamos posibles mensajes previos
+        }
+      },
+      error: (error) => {
+        // Si ocurre un error inesperado (problema de red, etc.)
+        this.errorMessage = 'Error al cargar el menú.';
+        this.successMessage = '';
+        console.error(error);    
+      }
+    })
   }
 
 
