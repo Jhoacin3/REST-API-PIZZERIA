365f872ad55afa55957995191f8717171277c56b
jhoacin21project <jhoacin21project@gmail.com>
2025-02-16 22:01:56 -0600

    add CORS middleware, update server port configuration for test environment, and export server instance. OJO!

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const { verifyToken } = require('./middlewares/auth.middleware');
+const cors = require('cors');
 
 // Importar rutas
 const rolRouter = require("./routes/rol.router.js");
@@ -14,12 +15,14 @@ const authRouter = require("./routes/auth/auth.js");
 const orderPaymentRouter = require("./routes/orderPaymentRouter.js");
 
 const app = express();
-const port = process.env.PORT || 3000;
+// const port = process.env.PORT || 3000;
+const port = process.env.NODE_ENV === 'test' ? 3000 : process.env.PORT || 4000;
+
 
 // Middleware general
 app.use(express.json());
 app.use(cookieParser());
-
+app.use(cors());
 // Archivos estáticos
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
@@ -32,6 +35,10 @@ app.use((req, res, next) => {
         '/apiPizza/auth/login',
         '/apiPizza/auth/register',
         '/apiPizza/auth/logout',
+        '/apiPizza/employees/getEmployees',
+        '/apiPizza/menu/getMenu',
+        '/apiPizza/categories/getCategory',
+        '/apiPizza/orderPayment/getTableNumbers'
     ];
 
     // Si la ruta es pública, permitir acceso sin token
@@ -53,6 +60,8 @@ app.use('/apiPizza/auth', authRouter);
 app.use('/apiPizza/orderPayment', orderPaymentRouter);
 
 // Servidor escuchando...
-app.listen(port, () => {
+const server = app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
+
+module.exports = {server, app};
