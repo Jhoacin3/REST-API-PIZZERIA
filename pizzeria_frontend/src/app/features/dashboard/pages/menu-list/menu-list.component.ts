288ecdfd46c8db503352aa95523a1037753d922a
jhoacin21project <jhoacin21project@gmail.com>
2025-02-21 13:54:12 -0600

    add modals for adding categories and menu items, enhance menu list component with category retrieval

--- pizzeria_frontend/src/app/features/dashboard/pages/menu-list/menu-list.component.ts
+++ pizzeria_frontend/src/app/features/dashboard/pages/menu-list/menu-list.component.ts
@@ -1,47 +1,76 @@
 import { Component, OnInit } from '@angular/core';
 import { MenuItemModel } from '../../../../core/models/menu-item.model';
+import { CategoryInterface } from '../../../../core/models/category-interface';
 import { MenuService } from '../../../../core/services/menu.service';
+import { CategoryService } from '../../../../core/services/category.service';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import {MaterialModule} from '../../../../material-module';
+import { MaterialModule } from '../../../../material-module';
 
 @Component({
   selector: 'app-menu-list',
-  imports: [CommonModule, RouterOutlet,MaterialModule],
+  imports: [CommonModule, RouterOutlet, MaterialModule],
   templateUrl: './menu-list.component.html',
-  styleUrl: './menu-list.component.css'
+  styleUrl: './menu-list.component.css',
 })
 export class MenuListComponent implements OnInit {
-  title = 'pizzeria_frontend';
   errorMessage = '';
   successMessage = '';
 
   menuItems: MenuItemModel[] = [];
-  //Constructor de la clase
-  constructor(private menuService: MenuService) { 
+  categoryItem: CategoryInterface[] = [];
 
-  }
+  constructor(
+    private menuService: MenuService,
+    private categoryService: CategoryService
+  ) {}
 
   ngOnInit(): void {
+    this.getMenusTable();
+    this.getCategoriesSelect();
+  }
+
+  private getMenusTable(): void {
     this.menuService.getMenuItems().subscribe({
       next: (response: any) => {
-        if (response.success) {
-          this.menuItems = response.data;
-          this.successMessage = response.message;
-          this.errorMessage = ''; // Limpiamos posibles errores previos
-        }else{
-          this.errorMessage = response.message;
-          this.successMessage = ''; // Limpiamos posibles mensajes previos
-        }
+        this.handleResponse(response, 'menuTable')
       },
       error: (error) => {
         // Si ocurre un error inesperado (problema de red, etc.)
         this.errorMessage = 'Error al cargar el menú.';
         this.successMessage = '';
-        console.error(error);    
-      }
-    })
+      },
+    });
+  }
+  private getCategoriesSelect(): void {
+    this.categoryService.getCategories().subscribe({
+      next: (response: any) => {
+      this.handleResponse(response,'category')
+      },
+      error: (error) => {
+        // Si ocurre un error inesperado (problema de red, etc.)
+        this.errorMessage = 'Error al cargar el menú.';
+        this.successMessage = '';
+        console.error(error);
+      },
+    });
   }
 
-
+  private handleResponse(response: any, type: any): void {
+    if (response.success) {
+      if (type === 'category') {
+        this.categoryItem = response.data;
+      }else if (type === 'menuTable') {
+        this.menuItems = response.data;
+        
+      } else {
+        
+      }
+      this.successMessage = response.message;
+      this.errorMessage = '';
+    } else {
+      this.errorMessage = response.message;
+      this.successMessage = '';
+    }
+  }
 }
