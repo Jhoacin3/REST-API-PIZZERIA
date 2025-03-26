d17875c47e836587bfb26b3def781be178fb662b
jhoacin21project <jhoacin21project@gmail.com>
2025-03-26 01:45:35 -0600

    add order list component and update routing to include orders page in sidebar

--- pizzeria_frontend/src/app/app.routes.ts
+++ pizzeria_frontend/src/app/app.routes.ts
@@ -10,12 +10,14 @@ import { LoginComponent } from './auth/login/login.component';
 import { authGuard } from './core/guards/auth.guard';
 import { PageErrorComponent } from './features/shared/page-error/page-error.component';
 import { OrderPaymentComponent } from './features/dashboard/pages/order-payment/order-payment.component';
+import { OrderListComponent } from './features/dashboard/pages/order-list/order-list.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'login', pathMatch: 'full' }, // Redirige a /login por defecto
     { path: 'home', component: HomeComponent, canActivate: [authGuard] }, // Ruta para el home
     { path: 'login', component: LoginComponent }, // Ruta para el home
     { path: 'menu', component: MenuListComponent, canActivate: [authGuard] }, // Ruta para el menú
+    { path: 'ordenes', component: OrderListComponent, canActivate: [authGuard] }, // Ruta para el menú
     { path: 'employees', component: EmployeesListComponent, canActivate: [authGuard] }, // Ruta para el menú
     { path: 'category', component: CategoriesListComponent, canActivate: [authGuard] }, // Ruta para el categorias
 
