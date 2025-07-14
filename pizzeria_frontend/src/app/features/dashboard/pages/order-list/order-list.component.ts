a227f25842bdab92ac690ca1664e2812f713ef1e
jhoacin21project <jhoacin21project@gmail.com>
2025-07-14 12:22:31 -0600

    agregando nuevo componente de order list component e interfaz

--- pizzeria_frontend/src/app/features/dashboard/pages/order-list/order-list.component.ts
+++ pizzeria_frontend/src/app/features/dashboard/pages/order-list/order-list.component.ts
@@ -1,11 +1,70 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { MaterialModule } from '../../../../material-module';
+import { FormsModule } from '@angular/forms';
+import { PaymentService } from '../../../../core/services/payment.service';
+import { OrdersInterface } from '../../../../core/models/orders-interface';
+import { OrderDetailsInterface } from '../../../../core/models/order-details-interface';
 
 @Component({
   selector: 'app-order-list',
-  imports: [],
+  imports: [CommonModule, RouterOutlet, MaterialModule, FormsModule],
   templateUrl: './order-list.component.html',
   styleUrl: './order-list.component.css'
 })
-export class OrderListComponent {
+export class OrderListComponent implements OnInit {
+  successMessage = "";
+  errorMessage = "";
+  ordersInterface: OrdersInterface[] = [];
+  orderDetailsRes: OrderDetailsInterface[] = [];
+  orderTotal: string = '';
+
+  constructor(private privateaymentService: PaymentService){}
+  ngOnInit(): void {
+    this.getOrderAll();
+  }
+  getOrderAll(): void{
+    this.privateaymentService.getOrderAll().subscribe({
+      next:(response: any) => {
+        if(response.success){
+          this.ordersInterface = response.data
+          this.successMessage = response.message
+          this.errorMessage = '';
+        }else{
+          this.handleError(response.error);
+        }
+        
+      },
+      error:()=> {
+        this.handleError("No se pudo obtener las ordenes, error de red")
+      }
+    })
+  }
+
+  getOrderDetails(id : number): void{
+    this.privateaymentService.getOrderDetails(id).subscribe({
+      next: (response : any) => {
+        if(response.success){
+          this.orderDetailsRes = response.data.result
+          this.orderTotal = response.data.total
+          this.successMessage = response.messages
+          this.errorMessage = "";
+        }else{
+          this.handleError(response.error);
+        }
+
+      },
+      error: () => {
+        this.handleError("No se pudo obtener los detalles de la orden, error de red")
+      }
+
+    })
+  }
+
+  private handleError(message: string): void {
+    this.errorMessage = message;
+    this.successMessage = '';
+  }
 
 }
--- pizzeria_frontend/src/app/features/dashboard/pages/order-payment/order-payment.component.ts
+++ pizzeria_frontend/src/app/features/dashboard/pages/order-payment/order-payment.component.ts
@@ -1,16 +1,27 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { MaterialModule } from '../../../../material-module';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { EmployeeInterface } from '../../../../core/models/employee-interface';
+import { CreateOrderInterface } from '../../../../core/models/create-order-interface';
 import { EmployeeService } from '../../../../core/services/employee.service';
+import { MenuService } from '../../../../core/services/menu.service';
 import { TablesListComponent } from '../../../../features/dashboard/pages/tables-list/tables-list.component';
 import { PaymentService } from '../../../../core/services/payment.service';
+import { MenuItemModel } from '../../../../core/models/menu-item.model';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { AlertService } from '../../../../services/alert.service.ts.service'; // Ajusta la ruta
 
 @Component({
   selector: 'app-order-payment',
-  imports: [CommonModule, MaterialModule, RouterOutlet, FormsModule, RouterLink],
+  imports: [CommonModule, MaterialModule, RouterOutlet, FormsModule, RouterLink,
+    MaterialModule, MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+  ],
   templateUrl: './order-payment.component.html',
   styleUrl: './order-payment.component.css'
 })
@@ -19,15 +30,51 @@ export class OrderPaymentComponent implements OnInit{
    errorMessage = "";
    selectEmployee= "";
    employeeItems : EmployeeInterface[] =[];
+
+   employees_id = 0;
+   menuDetails: CreateOrderInterface[] = [];
    selectedTableId: number | null = null;
+   selectedName: string = ''; // Nombre seleccionado del input
+   selectedId: number | null = null; // ID del insumo seleccionado
+    menu: MenuItemModel[] = [];
+  
     constructor(
       private employeService : EmployeeService,
-      private paymentService: PaymentService
+      private paymentService: PaymentService,
+      private menuService : MenuService,
+      private router: Router,
+      private alertService: AlertService   
     ){}
 
   ngOnInit(): void {
     this.getEmployeeSelect();
     this.getSelectedTableId();
+    this.getMenu();
+    this.getTotalOrder();
+  }
+  onSelectItem() {
+    const selectedItem = this.menu.find(item => item.name === this.selectedName);
+    this.selectedId = selectedItem ? selectedItem.id_menu : null;
+    if (selectedItem) {
+      const orderItem: CreateOrderInterface = {
+        id_menu: selectedItem.id_menu,
+        name: selectedItem.name,
+        id_category: selectedItem.id_category,
+        type: selectedItem.type,
+        amount: 1, //cambiar
+        unit_price: selectedItem.price,
+        description: selectedItem.description
+      };
+
+      this.menuDetails.push(orderItem);
+      console.log('Insumos seleccionados:', this.menuDetails);
+    }
+  }
+  getTotalOrder(): number {
+    if (this.menuDetails.length === 0) {
+      return 0;
+    }
+    return this.menuDetails.reduce((acc, item) => acc + item.unit_price * item.amount, 0);
   }
 
   getEmployeeSelect():void {
@@ -46,12 +93,68 @@ export class OrderPaymentComponent implements OnInit{
     })
   }
 
+  getMenu(): void{
+    this.menuService.getMenuItems().subscribe({
+      next: (response) => this.handleResponse(response),
+      error: () => this.handleError('Error al cargar el menú.'),
+    })
+  }
+
+  createOrder(): void{
+    // if(!this.employees_id || !this.selectedTableId)
+    //   {
+    //     this.errorMessage = 'Todos los campos son requeridos';
+    //     return;
+    //   }
+    
+
+    this.paymentService
+      .createOrden(this.employees_id, this.selectedTableId, this.menuDetails)
+      .subscribe({
+        next: (response) => {
+          if (response.success) {
+
+            console.log('Orden creada:', response.data);
+            this.alertService
+              .success(
+                '', response.messages
+              )
+              .then((result) => {
+                if (result.isConfirmed) {
+                  this.router.navigate(['/tables']);
+                }
+                this.router.navigate(['/tables']);
+              });
+            this.successMessage = response.messages;
+            this.errorMessage = '';
+
+          } else {
+            this.alertService.error('', response.error);
+          }
+        },
+        error: () => this.alertService.error('', 'Error al crear la orden')
+      });
+
+  }
+
   private handleError(message: string): void {
     this.errorMessage = message;
     this.successMessage = '';
   }
   private getSelectedTableId(): void {
     this.selectedTableId = this.paymentService.getTableId();
+  }
+
+
+  private handleResponse<T>(response: any): void {
+    if (response.success) {
+      this.menu = response.data;
+      this.successMessage = response.message;
+      this.errorMessage = '';
+    } else {
+      this.handleError(response.message);
+      this.successMessage = '';
+    }
   }
 
 }
